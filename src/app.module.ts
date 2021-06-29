d663da57d8347d164b1911f1e1b8bda39653e34b
Mathius <devachievem@gmail.com>
2021-06-29 09:19:36 -0700

    configure mongodb nestjs with docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
 import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
+console.log(process.env);
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController, UsersController],
-  providers: [AppService, UsersService],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      //Replace this line with the one Cluster > Connect > Connect your Application
+      `mongodb+srv://admin:admin@cluster0.aym4a.mongodb.net/nestjs-docker?retryWrites=true&w=majority`,
+    ),
+    UsersModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
